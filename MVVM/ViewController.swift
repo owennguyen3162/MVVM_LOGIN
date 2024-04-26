176ddcb7638ddccfa9a93d999c7c2887bcb764fa
owennguyen3162 <nguyentuan3162@gmail.com>
2024-04-26 23:10:34 +0700

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Nguyen Anh Tuan on 26/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
